6940a88adc4974d7611b1aa15406e1e2f9cc42fd
rafayodho <154619473+rafayodho@users.noreply.github.com>
2024-02-24 00:33:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let userName = prompt("what is your name");
+alert("Welcome " + userName);
+console.log("Username: " + userName);
+let num1 = prompt("enter first number");
+let num2 = prompt("enter second number");
+num1 = Number(num1);
+num2 = Number(num2);
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum);
